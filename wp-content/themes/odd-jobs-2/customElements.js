d53826a643cd98a064ea564a9ee65c81508c79bc
LebLeb03 <LebLeb03@users.noreply.github.com>
2024-04-09 03:38:32 +0000

    deploy: 54eb9802cc73a2175a28ab0a57cbd8869a3ede82

--- wp-content/themes/odd-jobs-2/customElements.js
+++ wp-content/themes/odd-jobs-2/customElements.js
@@ -6,10 +6,13 @@ class CopyrightYear extends HTMLElement{
 
 customElements.define("x-year", CopyrightYear);
 
-class TwoSidedMarket extends HTMLElement {
-connectedCallback(){
-this.innerHTML =  '<a href=”Shop for Odd Jobs”>Shop for Odd Jobs</a> &nbsp; <a href=”Odd Jobs Registration”>Odd Jobs Registration</a>';
-	}
+
+class TwoSidedMarket extends HTMLElement{
+	connectedCallback(){
+this.innerHTML ='<a href="Odd Job Form">Odd Job Form</a>&nbsp;<a href="Shop for Odd Jobs">Shop for Odd Jobs</a>' ;
 }
 
-customElements.define("x-twosided", TwoSidedMarket);
+
+}
+
+customElements.define("x-twosided", TwoSidedMarket)
